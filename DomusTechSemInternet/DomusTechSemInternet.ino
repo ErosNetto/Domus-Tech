70276e37501b0c647dcc9d5fe3021b15c1de861a
Eros Netto Antunes <erosnetto1002@gmail.com>
2024-10-23 18:11:10 -0300

    ATT

--- DomusTechSemInternet/DomusTechSemInternet.ino
+++ DomusTechSemInternet/DomusTechSemInternet.ino
@@ -1,8 +1,4 @@
-#include <WiFi.h>                         // Biblioteca do WiFi
-#include <WiFiManager.h>                  // Biblioteca do WiFiManager
 #include <LiquidCrystal_I2C.h>            // Biblioteca do LCD
-#include <ESPAsyncWebServer.h>            // Biblioteca para criar um servidor
-#include <AsyncTCP.h>                     // Biblioteca para o AsyncWebServer
 #include <ESP32Servo.h>                   // Biblioteca para o Servo Motor
 
 
@@ -41,9 +37,6 @@ IPAddress local_IP(192, 168, 18, 85);     // IP fixo
 IPAddress gateway(192, 168, 18, 1);       // Gateway na mesma sub-rede
 IPAddress subnet(255, 255, 255, 0);       // Máscara de sub-rede padrão
 
-AsyncWebServer server(80);
-WiFiManager wm;                           // Instância do WiFiManager
-
 // ------------- Variáveis globais -------------
 bool ledState[] = {0, 0, 0};              // Variável para controlar se os leds estão ligados ou desligados
 bool alarmeLigado = false;                // Variável para controlar se o alarme está ligado ou desligado
@@ -78,163 +71,12 @@ void setup() {
     lcd.backlight();                // Liga a luz de fundo do LCD
     mostrarNoLCD("   Domus Tech   ", "Carregando...");
     delay(500);
-
-    // Inicia as configurações do Wi-Fi
-    configurarWiFi();
-    delay(500);
-
-
-
-
-    // ------------- CONFIGURAÇÃO para as requisições HTTP ------------- //
-    // ------------- Rota para controlar LEDs ( http://192.168.18.85/led?ledNum=1&state=1 ) -------------
-    server.on("/led", HTTP_GET, [](AsyncWebServerRequest *request) {
-        if (request->hasParam("ledNum") && request->hasParam("state")) {
-            int ledNumero = request->getParam("ledNum")->value().toInt();
-            int ledStateRequest = request->getParam("state")->value().toInt();
-            int pin;
-
-            if (ledNumero == 1) {
-                pin = ledPin1;
-            } else if (ledNumero == 2) {
-                pin = ledPin2;
-            } else if (ledNumero == 3) {
-                pin = ledPin3;
-            } else {
-                request->send(400, "text/plain", "Número do LED invalido.");
-                return;
-            }
-
-            Serial.println("LED " + String(ledNumero) + ((ledStateRequest == HIGH) ? ": ON" : ": OFF"));
-            
-            digitalWrite(pin, ledStateRequest); // Liga ou desliga o LED
-            ledState[ledNumero - 1] = ledStateRequest; // Atualiza o estado do LED no array
-
-            String state = (ledStateRequest == HIGH) ? "ON" : "OFF";
-            request->send(200, "text/plain", "LED " + String(ledNumero) + " esta " + state);
-        } else {
-            request->send(400, "text/plain", "Parâmetro 'ledNum' ou 'state' ausentes");
-        }
-    });
-
-    // ------------- Rota para ligar e desligar o alarme ( http://192.168.18.85/alarme?state=1 ) -------------
-    server.on("/alarme", HTTP_GET, [](AsyncWebServerRequest *request) {
-        if (request->hasParam("state")) {
-            int alarmeState = request->getParam("state")->value().toInt();
-
-            // Ativa ou desativa o alarme
-            if (alarmeState == 1 && !alarmeLigado) {
-                alarmeLigado = true;
-                alarmeAtivo = false;
-                mostrarNoLCD("Alarme ligado", "remotamente");
-                somAlarmeLigando();
-                Serial.println("\nAlarme ligado remotamente.");
-            } else if (alarmeState == 0 && alarmeLigado) {
-                alarmeLigado = false;
-                alarmeAtivo = false; 
-                mostrarNoLCD("Alarme desligado", "remotamente");
-                pararSomAlarme();
-                delay(200);
-                somAlarmeDesligando();
-                delay(200);
-                pararSomAlarme();
-                Serial.println("\nAlarme desligado remotamente.");
-            } else {
-                request->send(400, "text/plain", "Ação inválida.");
-                return;
-            }
-
-            digitalWrite(ledAlarmeLigado, alarmeLigado ? HIGH : LOW);
-
-            String state = alarmeLigado ? "ligado" : "desligado";
-            request->send(200, "text/plain", "O alarme está " + state);
-        } else {
-            request->send(400, "text/plain", "Parâmetro 'state' ausente");
-        }
-    });
-
-    // ------------- Rota para ativar ou desativar o sensor de movimento ( http://192.168.18.85/sensormovimento?state=1 ) -------------
-    server.on("/sensormovimento", HTTP_GET, [](AsyncWebServerRequest *request) {
-        if (request->hasParam("state")) {
-            int sensorState = request->getParam("state")->value().toInt();
-
-            // Ativa ou desativa o sensor de movimento
-            if (sensorState == 1) {
-                sensorMovimentoAtivado = true;
-                mostrarNoLCD("-- Sensor PIR --", "Ativado");
-                somAtivaSensorMovimento();
-                Serial.println("\nSensor PIR ativado remotamente.");
-            } else if (sensorState == 0) {
-                sensorMovimentoAtivado = false;
-                mostrarNoLCD("-- Sensor PIR --", "Desativado");
-                somDesativaSensorMovimento();
-                Serial.println("\nSensor PIR desativado remotamente.");
-            } else {
-                request->send(400, "text/plain", "Ação inválida.");
-                return;
-            }
-
-            String state = sensorMovimentoAtivado ? "ativado" : "desativado";
-            request->send(200, "text/plain", "O alarme está " + state);
-        } else {
-            request->send(400, "text/plain", "Parâmetro 'state' ausente");
-        }
-    });
-
-    // ------------- Rota para controlar o portão ( http://192.168.18.85/portao?state=1 ) -------------
-    server.on("/portao", HTTP_GET, [](AsyncWebServerRequest *request) {
-        if (request->hasParam("state")) {
-            int portaoState = request->getParam("state")->value().toInt();
-
-            if (portaoState == 1 && portaoAberto == false) {
-                abrirPortao();
-                Serial.println("\nPortão aberto remotamente.");
-                mostrarNoLCD("Portao aberto", "remotamente");
-            } else if (portaoState == 0 && portaoAberto == true) {
-                fecharPortao();
-                Serial.println("\nPortao fechado remotamente.");
-                mostrarNoLCD("Portao fechado", "remotamente");
-            } else {
-                request->send(400, "text/plain", "Ação inválida.");
-            }
-
-            String state = (portaoState) ? "aberto." : "fechado.";
-            request->send(200, "text/plain", "O portão está " + state);
-        } else {
-            request->send(400, "text/plain", "Parâmetro 'state' ausente");
-        }
-    });
-
-    // ------------- Rota para obter o status dos dispositivos ( http://192.168.18.85/status ) -------------
-    server.on("/status", HTTP_GET, [](AsyncWebServerRequest *request) {
-        String status = "";
-        status += "LED1: " + String(digitalRead(ledPin1)) + "\n";
-        status += "LED2: " + String(digitalRead(ledPin2)) + "\n";
-        status += "LED3: " + String(digitalRead(ledPin3)) + "\n";   
-        status += (alarmeLigado) ? "Alarme esta ligado\n" : "Alarme esta desligado\n";
-        status += (portaoAberto) ? "Portao esta aberto\n" : "Portao esta fechado\n";
-        status += (alarmeAtivo) ? "Alarme disparado" : "Alarme nao esta disparado";
-
-        Serial.println("\nEnviando status.");
-        request->send(200, "text/plain", status);
-    });
-
-    server.begin();
 }
 
 void loop() {
     loopAlarme();   // Controle do sistema de alarme
     loopPortao();   // Controle do sistema de portão 
     loopLeds();     // Controle do sistema de leds
-
-    // Verifica continuamente se o botão foi pressionado para resetar o Wi-Fi
-    if (digitalRead(btnResetWifi) == LOW) {
-        Serial.println("\nBotão pressionado. Resetando configurações de Wi-Fi...");
-        mostrarNoLCD("Resetando Wi-Fi", "Reiniciando...");
-        wm.resetSettings();
-        delay(3000);        
-        ESP.restart();
-    }
 
     // Reinicia a ESP32
     if (digitalRead(btnResetESP32) == LOW) {
@@ -244,108 +86,6 @@ void loop() {
         ESP.restart();
     }
 }
-
-
-
-
-// ------------- CONFIGURAÇÃO DE WI-FI ------------- //
-// Inicia as configurações de Wi-Fi
-void configurarWiFi() {
-    // Verifica se há redes Wi-Fi salvas no WiFiManager
-    if (hasSavedNetworks()) {
-        Serial.println("Wi-Fi salvo encontrado. Tentando conectar...");
-        mostrarNoLCD("Wi-Fi salvo", "Conectando...");
-        delay(2000);
-
-        // configurarIPFixo(); // Configura o IP fixo antes de tentar conectar
-
-        WiFi.begin(); // Tenta conectar ao Wi-Fi salvo
-
-        // Aguarda até conectar ou até o timeout de 10 segundos
-        unsigned long startAttemptTime = millis();
-        while (WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < 10000) {
-            yield();
-            Serial.print(".");
-        }
-
-        if (WiFi.status() == WL_CONNECTED) {
-            Serial.println("Conectado ao Wi-Fi! " + WiFi.SSID());
-            mostrarNoLCD("WiFi conectado!", WiFi.SSID());
-            delay(3000);
-            ipTexto();
-        } else {
-            Serial.println("Falha ao conectar ao Wi-Fi salvo.");
-            mostrarNoLCD("Falha na conexao", "Reiniciando...");
-            delay(3000);
-            ESP.restart(); // Reinicia a ESP32
-        }
-    } else {
-        Serial.println("Nenhuma rede Wi-Fi salva foi encontrada.");
-        mostrarNoLCD("  Nenhuma rede  ", "  Wi-Fi  salva  ");
-        delay(3000);
-        iniciarModoConfiguracaoWiFi();
-    }
-}
-
-// Função para verificar se há redes Wi-Fi salvas no WiFiManager
-bool hasSavedNetworks() {
-    WiFi.mode(WIFI_STA); // Garante que estamos no modo estação
-    WiFi.begin();        // Inicia o WiFi com credenciais salvas, se houver
-    delay(200);
-
-    // Verifica se temos um SSID salvo
-    return (WiFi.SSID() != "");
-}
-
-// Função para iniciar o modo de configuração de Wi-Fi
-void iniciarModoConfiguracaoWiFi() {
-    Serial.println("Entrando em modo de configuração Wi-Fi...");
-    mostrarNoLCD("Entrando em modo", "de config Wi-Fi");
-    delay(3000);
-
-    mostrarNoLCD("Por favor, faca", "a configuracao");
-    delay(500);
-    
-    // Inicia o WiFiManager para modo de configuração
-    bool res = wm.autoConnect("Domus_Tech", "123123123");
-    if (!res) {
-        Serial.println("Falha na configuração do Wi-Fi");
-        mostrarNoLCD("Falha Config", "Reiniciando...");
-        delay(4000);
-        ESP.restart();
-    } else {
-        Serial.println("Wi-Fi Configurado com sucesso! " + WiFi.SSID());
-        mostrarNoLCD("WiFi conectado!", WiFi.SSID());
-        delay(3000);
-        // configurarIPFixo();
-        delay(500);
-        ipTexto();
-    }
-}
-
-// Função para configurar o IP fixo
-// void configurarIPFixo() {
-//     if (!WiFi.config(local_IP, gateway, subnet)) {
-//         delay(500);
-//         Serial.println("Falha ao configurar o IP estático.");
-//         mostrarNoLCD("Falha ao config", "o IP estatico");
-//     }
-// }
-
-// Função para escrever o IP da ESP no terminal e no LCD 
-// void ipTexto() {
-//     Serial.println("IP estático configurado: " + local_IP.toString());
-//     mostrarNoLCD("IP atribuido:", local_IP.toString());
-// }
-
-void ipTexto() {
-    // Exibe o IP atribuído pela ESP diretamente
-    Serial.println("IP atribuído pela ESP: " + WiFi.localIP().toString());
-
-    // Exibe o IP no LCD diretamente
-    mostrarNoLCD("IP atribuido:", WiFi.localIP().toString());
-}
-
 
 
 
